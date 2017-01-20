ebeb1a0f84d7423887a764fc0eaa2d5ec9c56f1b
Jason Zhang <zhang.j@berkeley.edu>
2017-01-19 20:01:07 -0800

    hw01 draft

---
+++ hw/hw01/tests/q1.py
@@ -0,0 +1,33 @@
+test = {
+'name': "Question",
+'points': 1,
+'suites': [
+{
+'cases': [
+
+{
+'code': r"""
+
+""",
+'hidden': False,
+'locked': False
+}
+,
+{
+'code': r"""
+>>> True
+True
+
+""",
+'hidden': False,
+'locked': False
+}
+
+],
+'scored': True,
+'setup': '',
+'teardown': '',
+'type': 'doctest'
+}
+]
+}
