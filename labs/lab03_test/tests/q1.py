f8fa8e2035864226636bd13556de92a664a6e2c3
Jason Zhang <zhang.j@berkeley.edu>
2017-02-02 20:36:20 -0800

    lab03_test

---
+++ labs/lab03_test/tests/q1.py
@@ -0,0 +1,25 @@
+test = {
+'name': "Question",
+'points': 1,
+'suites': [
+{
+'cases': [
+
+{
+'code': r"""
+>>> I_saved_my_file
+True
+
+""",
+'hidden': False,
+'locked': False
+}
+
+],
+'scored': True,
+'setup': '',
+'teardown': '',
+'type': 'doctest'
+}
+]
+}
