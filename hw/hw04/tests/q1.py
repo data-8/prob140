f841fc64e73f5f73e199768d40ebf8b09af8fbf4
Jason Zhang <zhang.j@berkeley.edu>
2017-02-08 22:29:38 -0800

    hw04

---
+++ hw/hw04/tests/q1.py
@@ -0,0 +1,25 @@
+test = {
+'name': "Question",
+'points': 1,
+'suites': [
+{
+'cases': [
+
+{
+'code': r"""
+>>> True == True
+True
+
+""",
+'hidden': False,
+'locked': False
+}
+
+],
+'scored': True,
+'setup': '',
+'teardown': '',
+'type': 'doctest'
+}
+]
+}
